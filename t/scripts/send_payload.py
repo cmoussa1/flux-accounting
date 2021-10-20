0068ae89460c2e764ede82477cc3f3d31c09486e
Christopher Moussa <moussa1@llnl.gov>
2021-10-20 08:40:19 -0700

    send_payload.py: add QoS data loop
    Add another loop to send_payload.py to send
    QoS information from the JSON file to the
    plugin.

--- t/scripts/send_payload.py
+++ t/scripts/send_payload.py
@@ -11,3 +11,6 @@ with open(sys.argv[1]) as data_file:
 
 for user in data["users"]:
     h.rpc("job-manager.mf_priority.get_users", user).get()
+
+for qos in data["qos"]:
+    h.rpc("job-manager.mf_priority.get_qos", qos).get()
